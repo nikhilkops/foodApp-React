c14f7167a4933061e92abff30270019e7f388a77
nikhil <nikhil.kops@gmail.com>
2023-06-13 12:58:03 +0530

    How implemented

---
+++ src/components/How/app_images.js
@@ -0,0 +1,7 @@
+const images={
+   c1: "https://i.ibb.co/fDN1cj9/app-screen-1.png",
+   c2 :"https://i.ibb.co/fDBzZLL/app-screen-2.png",
+   c3: "https://i.ibb.co/Qp26FZN/app-screen-3.png" 
+}
+
+export default images
