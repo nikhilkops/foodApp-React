f1916cc1bd4d8cf2a91f3acd740fa866ac25a1f1
nikhil <nikhil.kops@gmail.com>
2023-06-13 11:58:17 +0530

    added hero

--- src/components/Hero/customerImages.js
+++ src/components/Hero/customerImages.js
@@ -1,12 +1,12 @@
 const images = {
-    c1: require("../content/img/customers/ben.jpg"),
-    c2: require("../content/img/customers/customer-1.jpg"),
-    c3: require("../content/img/customers/customer-2.jpg"),
-    c4: require("../content/img/customers/customer-3.jpg"),
-    c5: require("../content/img/customers/customer-4.jpg"),
-    c6: require("../content/img/customers/customer-5.jpg"),
-    c7: require("../content/img/customers/customer-6.jpg"),
+    c1: "https://i.ibb.co/hdh7qqq/customer-1.jpg",
+    c2:  "https://i.ibb.co/3BPWpvZ/customer-2.jpg",
+    c3:  "https://i.ibb.co/1KrYZpb/customer-3.jpg",
+    c4:  "https://i.ibb.co/2vY9hWm/customer-4.jpg",
+    c5:  "https://i.ibb.co/vY59y4q/customer-5.jpg",
+    c6:  "https://i.ibb.co/hV7bz59/customer-6.jpg",
+ 
   };
   
-  export default images;
-  
+  export default images; 
+
