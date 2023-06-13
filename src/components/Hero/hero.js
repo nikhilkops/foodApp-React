d97c27aad5f86c3a29ef63c9a9a40132904b3274
nikhil <nikhil.kops@gmail.com>
2023-06-13 12:06:34 +0530

    fixed buttons

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -4,22 +4,22 @@ import {NavLink} from "react-router-dom";
 import customer_Images from "./customerImages"
 function Hero() {
   return ( 
-    <section class="section-hero">
-    <div class="hero">
-      <div class="hero-text-box">
-        <h1 class="heading-primary">
+    <section className="section-hero">
+    <div className="hero">
+      <div className="hero-text-box">
+        <h1 className="heading-primary">
           A healthy meal delivered to your door, every single day
         </h1>
-        <p class="hero-description">
+        <p className="hero-description">
           The smart 365-days-per-year food subscription that will make you
           eat healthy again. Tailored to your personal tastes and
           nutritional needs.
         </p>
-        <NavLink  to="/" class="btn btn--full margin-right-sm"
+        <NavLink  to="/" className="btn btn--full margin-right-sm"
           >Start eating well</NavLink>
-        <NavLink to="/" class="btn btn--outline">Learn more &darr;</NavLink>
-        <div class="delivered-meals">
-          <div class="delivered-imgs">
+        <NavLink to="/" className="btn btn--outline">Learn more &darr;</NavLink>
+        <div className="delivered-meals">
+          <div className="delivered-imgs">
           {Object.values(customer_Images).map((value, index) => {
                   return (
                     <img
@@ -31,16 +31,16 @@ function Hero() {
                   );
                 })}
           </div>
-          <p class="delivered-text">
+          <p className="delivered-text">
             <span>250,000+</span> meals delivered last year!
           </p>
         </div>
       </div>
-      <div class="hero-img-box">
+      <div className="hero-img-box">
         <img
           src="https://i.ibb.co/KFpFDP2/hero.png" 
 
-          class="hero-img"
+          className="hero-img"
           alt="Woman enjoying food, meals in storage container, and food bowls on a table"
         />
       </div>
