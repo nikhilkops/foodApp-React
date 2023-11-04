ba371f360cf7b0f78031869ae4edc347c284bc56
nikhilkops <nikhil.kops@gmail.com>
2023-11-04 15:07:36 +0530

    deplpoyed backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api", // The URL path to proxy
     createProxyMiddleware({
-      target: "http://localhost:5100", // The backend server URL
+      target: "https://foodapp-backend-z289.onrender.com/", // The backend server URL
       changeOrigin: true, // Changes the origin of the host header to the target URL
     })
   );
