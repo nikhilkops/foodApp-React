d44052b2c18ffcdeb836144e6a40aec3eb3c6239
nikhilkops <nikhil.kops@gmail.com>
2023-11-04 20:08:22 +0530

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) { 
+module.exports = function (app) {
   app.use(
-    "/api", // The URL path to proxy
+    "/api",
     createProxyMiddleware({
-      target: "https://foodapp-backend-z289.onrender.com/", // The backend server URL
-      changeOrigin: true, // Changes the origin of the host header to the target URL
+      target: "http://localhost:5100",
+      changeOrigin: true,
     })
   );
 };
--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const customFetch = axios.create({
-  baseURL: "https://foodapp-backend-b38s.onrender.com/api/v1",
+  baseURL: "/api/v1",
 });
 
 export default customFetch;
